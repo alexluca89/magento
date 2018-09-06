61c659c485d5152678ac203cfb8169ae89460926
alexandru luca <alexandruluca@alexandrus-MacBook-Pro.local>
2018-09-06 12:49:24 +0100

    fixed code style errors and warnings

--- Controller/Adminhtml/Config/Index.php
+++ Controller/Adminhtml/Config/Index.php
@@ -2,27 +2,25 @@
 
 namespace Xsellco\Edesk\Controller\Adminhtml\Config;
 
-
 use Magento\Backend\App\Action;
 
 class Index extends \Magento\Backend\App\Action
 {
-	/**
-	 * @var \Magento\Framework\View\Result\PageFactory
-	 */
-	protected $_resultPageFactory;
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    private $resultPageFactory;
 
-	public function __construct(
-		Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $resultPageFactory
-	)
-	{
-		parent::__construct($context);
-		$this->_resultPageFactory = $resultPageFactory;
-	}
+    public function __construct(
+        Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
 
-	public function execute()
-	{
-		return $this->_resultPageFactory->create();
-	}
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
 }
